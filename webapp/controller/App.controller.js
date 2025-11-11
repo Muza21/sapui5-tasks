5522f147b08b11fe63e8e21f021a839ace85d411
Muza21 <lasha.muzashvili1@gmail.com>
2025-11-11 18:04:53 +0400

    feat: sort by fields

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,6 +1,10 @@
 sap.ui.define(
-  ["sapui5-tasks/controller/Base.controller", "sapui5-tasks/model/models"],
-  function (BaseController, models) {
+  [
+    "sapui5-tasks/controller/Base.controller",
+    "sapui5-tasks/model/models",
+    "sap/m/MessageToast",
+  ],
+  function (BaseController, models, MessageToast) {
     "use strict";
 
     return BaseController.extend("sapui5-tasks.controller.App", {
@@ -39,6 +43,50 @@ sap.ui.define(
           aBooks.splice(iIndex, 1);
           oModel.refresh();
         }
+      },
+
+      onOpenSortDialog: async function () {
+        this.oDialog ??= await this.loadFragment({
+          name: "sapui5-tasks.view.SortDialog",
+        });
+
+        this.oDialog.open();
+      },
+
+      onCloseDialog: function () {
+        this.byId("SortDialog").close();
+      },
+
+      onCloseDialogWithSort: function () {
+        if (!this.byId("SortDialog")) return;
+
+        const oFieldCombo = this.byId("sortField");
+        const oOrderCombo = this.byId("sortOrder");
+        const sField = oFieldCombo.getSelectedKey();
+        const sOrder = oOrderCombo.getSelectedKey();
+        const oModel = this.getModel("booksModel");
+        const aBooks = oModel.getProperty("/books");
+        if (sField) {
+          aBooks.sort((a, b) => {
+            let valA = a[sField];
+            let valB = b[sField];
+            if (typeof valA === "number" && typeof valB === "number") {
+              return sOrder === "asc" ? valA - valB : valB - valA;
+            }
+            if (sField === "ReleaseDate") {
+              valA = new Date(valA);
+              valB = new Date(valB);
+              return sOrder === "asc" ? valA - valB : valB - valA;
+            }
+            valA = valA.toString().toLowerCase();
+            valB = valB.toString().toLowerCase();
+            if (valA < valB) return sOrder === "asc" ? -1 : 1;
+            if (valA > valB) return sOrder === "asc" ? 1 : -1;
+            return 0;
+          });
+        }
+        oModel.refresh();
+        this.byId("SortDialog").close();
       },
     });
   }
