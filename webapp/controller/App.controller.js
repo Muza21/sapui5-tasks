6d97f0be6c7f721a9220c9324079b77799155b95
Muza21 <lasha.muzashvili1@gmail.com>
2025-11-11 17:14:52 +0400

    feat: create and delete a book

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,6 +8,38 @@ sap.ui.define(
         const oBooksModel = models.createBooksModel();
         this.setModel(oBooksModel, "booksModel");
       },
+
+      onAddRecord: function () {
+        const oModel = this.getModel("booksModel");
+        const aBooks = oModel.getProperty("/books");
+        const iMaxIdNum = aBooks.reduce((iMax, oBook) => {
+          const iNum = parseInt(oBook.ID.slice(1), 10);
+          return Math.max(iMax, iNum);
+        }, 0);
+        const sNextId = "b" + String(iMaxIdNum + 1).padStart(3, "0");
+        aBooks.push({
+          ID: sNextId,
+          Name: "",
+          Author: "",
+          Genre: "",
+          ReleaseDate: "",
+          AvailableQuantity: 0,
+        });
+        oModel.refresh();
+      },
+
+      onDeleteRecord: function () {
+        const oTable = this.byId("booksTable");
+        const oModel = this.getModel("booksModel");
+        const aBooks = oModel.getProperty("/books");
+        const oSelected = oTable.getSelectedItem();
+        if (oSelected) {
+          const oContext = oSelected.getBindingContext("booksModel");
+          const iIndex = parseInt(oContext.getPath().split("/").pop(), 10);
+          aBooks.splice(iIndex, 1);
+          oModel.refresh();
+        }
+      },
     });
   }
 );
