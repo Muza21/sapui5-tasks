fd74a25005ed219d74e392af9ba1ee1b84f3fd70
Muza21 <lasha.muzashvili1@gmail.com>
2025-11-13 18:13:41 +0400

    feat: add book form

--- project1/webapp/controller/Main.controller.js
+++ project1/webapp/controller/Main.controller.js
@@ -4,8 +4,9 @@ sap.ui.define(
     "project1/model/books",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel",
   ],
-  function (BaseController, books, Filter, FilterOperator) {
+  function (BaseController, books, Filter, FilterOperator, JSONModel) {
     "use strict";
 
     return BaseController.extend("project1.controller.App", {
@@ -17,21 +18,14 @@ sap.ui.define(
       onAddRecord: function () {
         const oModel = this.getModel("booksModel");
         const aBooks = oModel.getProperty("/books");
-        const iMaxIdNum = aBooks.reduce((iMax, oBook) => {
-          const iNum = parseInt(oBook.ID.slice(1), 10);
-          return Math.max(iMax, iNum);
-        }, 0);
-        const sNextId = "b" + String(iMaxIdNum + 1).padStart(3, "0");
+        const oBookData = this.oDialog.getModel("book").getData();
         aBooks.push({
-          ID: sNextId,
-          Name: "",
-          Author: "",
-          Genre: "",
-          ReleaseDate: "",
-          AvailableQuantity: 0,
+          ID: Date.now(),
+          ...oBookData,
           editable: false,
         });
         oModel.refresh();
+        this.oDialog.close();
       },
 
       onDeleteRecord: function () {
@@ -109,6 +103,23 @@ sap.ui.define(
 
       onCloseConfirmDialog: function () {
         this.byId("ConfirmDialog").close();
+      },
+
+      onOpenFormDialog: async function () {
+        this.oDialog ??= await this.loadFragment({
+          name: "project1.view.FormDialog",
+        });
+
+        const oFormModel = new JSONModel({
+          Name: "",
+          Author: "",
+          Genre: "",
+          ReleaseDate: "",
+          AvailableQuantity: "",
+        });
+
+        this.oDialog.setModel(oFormModel, "book");
+        this.oDialog.open();
       },
     });
   }
